5fd96acee823d28e87a77d2fe5693a6f39d61844
ozu6 <ozu6.hh@gmail.com>
2025-04-07 16:46:27 +0900

    Add files via upload

---
+++ sample 1/sketch.js
@@ -0,0 +1,23 @@
+// Click on the canvas to begin detecting key presses.
+
+function setup() {
+  createCanvas(100, 100);
+
+  describe(
+    'A gray square with a white square at its center. The white square turns black when the user presses a key.'
+  );
+}
+
+function draw() {
+  background(200);
+
+  // Style the square.
+  if (keyIsPressed === true) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+
+  // Draw the square.
+  square(25, 25, 50);
+}
